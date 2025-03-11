81fe9f9f1847e3a52e9daf5e715eee0885e3cbd3
ashokkumar272 <ashokkumar.vg2@outlook.com>
2025-03-11 09:25:12 +0530

    rapid api added

--- backend/controllers/findTrains.js
+++ backend/controllers/findTrains.js
@@ -1,9 +1,10 @@
 const trains = require("../assets/trainData");
+// import axios from 'axios';
 
 const findTrains = async (req, res) => {
   const { from, to, train_date } = req.query;
 
-  // Check if required query parameters are provided
+  
   if (!from || !to || !train_date) {
     return res.status(400).json({
       status: false,
@@ -11,6 +12,34 @@ const findTrains = async (req, res) => {
     });
   }
 
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://irctc1.p.rapidapi.com/api/v3/trainBetweenStations',
+//   params: {
+//     fromStationCode: 'BVI',
+//     toStationCode: 'NDLS',
+//     dateOfJourney: '2025-03-13'
+//   },
+//   headers: {
+//     'x-rapidapi-key': '9f433ad860msh107f7a78693edd4p100c87jsn0cc310348312',
+//     'x-rapidapi-host': 'irctc1.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+
+  // Check if required query parameters are provided
+  
+  
+  
   const filteredTrains = trains.filter(
     (train) =>
       train.train_date === train_date &&
